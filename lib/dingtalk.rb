af917b54a209024f814e54f982b8f083c3ad5b17
Jeff Lai <winfield301@gmail.com>
2016-02-29 15:47:42 +0800

    Add redis & rest-client as dependencies

--- lib/dingtalk.rb
+++ lib/dingtalk.rb
@@ -1,6 +1,11 @@
 require "dingtalk/version"
+require "dingtalk/config"
 require "dingtalk/pkcs7_encoder"
 require "dingtalk/prpcrypt"
+require "dingtalk/api"
+
+require 'redis'
+require "rest-client"
 
 module Dingtalk
   ENDPOINT = "https://oapi.dingtalk.com"
