55b4368cd85e7c6eb0f59c6ea6d3da5817721a4c
Jeff Lai <winfield301@gmail.com>
2016-03-08 19:08:11 +0800

    Fix user apis

--- lib/dingtalk/api/user.rb
+++ lib/dingtalk/api/user.rb
@@ -2,11 +2,11 @@ module Dingtalk
   module Api
     class User < Base
       def get_user_info(code)
-        http_get('getuserinfo', { code: code })
+        http_get("getuserinfo?access_token=#{access_token}&code=#{code}")
       end
 
       def get(userid)
-        http_get('get', { userid: userid })
+        http_get("get?access_token=#{access_token}&userid=#{userid}")
       end
 
       private
