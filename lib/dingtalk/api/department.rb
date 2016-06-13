c32545d4cc6629a5f6f2e8dd43e43816061e9ee1
Jeff Lai <winfield301@gmail.com>
2016-06-13 16:57:37 +0800

    Add more department apis

--- lib/dingtalk/api/department.rb
+++ lib/dingtalk/api/department.rb
@@ -5,6 +5,18 @@ module Dingtalk
         http_get("list?access_token=#{access_token}")
       end
 
+      def create(params)
+        http_post("create?access_token=#{access_token}", params)
+      end
+
+      def update(params)
+        http_post("update?access_token=#{access_token}", params)
+      end
+
+      def delete(id)
+        http_get("delete?access_token=#{access_token}&id=#{id}")
+      end
+
       private
         def base_url
           'department'
