ddc4544f2265867f19c60e1a09330c43292867b7
Jeff Lai <winfield301@gmail.com>
2016-08-02 11:32:03 +0800

    Fix bug

--- lib/dingtalk/api/call_back.rb
+++ lib/dingtalk/api/call_back.rb
@@ -1,16 +1,11 @@
 module Dingtalk
   module Api
     class CallBack < Base
-      def initialize
-        @suite_token = Dingtalk.suite_token
-        @suite_aes_key = Dingtalk.suite_aes_key
-      end
-
       def register_call_back(call_back_tag = [], url)
         params = {
           call_back_tag: call_back_tag,
-          token: @suite_token,
-          aes_key: @suite_aes_key,
+          token: Dingtalk.suite_token,
+          aes_key: Dingtalk.suite_aes_key,
           url: url
         }
         http_post("register_call_back?access_token=#{access_token}", params)
