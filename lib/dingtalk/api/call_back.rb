a75d00e1911ccbbfca0666620d8c53a28d43a720
Jeff Lai <winfield301@gmail.com>
2016-08-02 11:49:39 +0800

    Add rest of the call back apis

--- lib/dingtalk/api/call_back.rb
+++ lib/dingtalk/api/call_back.rb
@@ -1,17 +1,33 @@
 module Dingtalk
   module Api
     class CallBack < Base
-      def register_call_back(call_back_tag = [], url)
-        params = {
-          call_back_tag: call_back_tag,
-          token: Dingtalk.suite_token,
-          aes_key: Dingtalk.suite_aes_key,
-          url: url
-        }
-        http_post("register_call_back?access_token=#{access_token}", params)
+      def register_call_back(call_back_tag, url)
+        http_post("register_call_back?access_token=#{access_token}", params(call_back_tag, url))
+      end
+
+      def update_call_back(call_back_tag, url)
+        http_post("update_call_back?access_token=#{access_token}", params(call_back_tag, url))
+      end
+
+      def get_call_back
+        http_get("get_call_back?access_token=#{access_token}")
+      end
+
+
+      def delete_call_back
+        http_get("delete_call_back?access_token=#{access_token}")
       end
 
       private
+        def params(call_back_tag, url)
+          {
+            call_back_tag: call_back_tag,
+            token: Dingtalk.suite_token,
+            aes_key: Dingtalk.suite_aes_key,
+            url: url
+          }
+        end
+
         def base_url
           'call_back'
         end
