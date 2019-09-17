843a2031175191de0ec4d82491237bfc8ea2113f
Tao Huang <taohms@gmail.com>
2019-09-17 22:30:44 +0800

    Remove configure

--- lib/dingtalk.rb
+++ lib/dingtalk.rb
@@ -13,11 +13,3 @@ module Dingtalk
   ENDPOINT = "https://oapi.dingtalk.com"
   # Your code goes here...
 end
-
-Dingtalk.configure do |config|
-  config.suite_key = 'suiteeazsojmzckxgkw4a'
-  config.suite_secret = 'blCId7p9Iy44SyNc2RviaV66-yM7bxis55T197_nIjns1JGJ0CnBSgarLT4lUGK7'
-  config.suite_aes_key = 'wsuph070rt8ni8ll8yoe6ccwktz5uk94vnqtnach4zl'
-  config.suite_token = 'token'
-  config.redis = Redis.new
-end
