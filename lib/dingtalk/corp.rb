47545c86a1ada5d1e91c73a7446af40af36f8c91
Tao Huang <taohms@gmail.com>
2019-09-21 15:44:55 +0800

    Add more properties to corp

--- lib/dingtalk/corp.rb
+++ lib/dingtalk/corp.rb
@@ -1,11 +1,12 @@
 module Dingtalk
   class Corp
-    attr_accessor :corp_id, :isv_mode, :corp_secret
+    attr_accessor :corp_id, :isv_mode, :corp_secret, :permanent_code
 
     def initialize(corp_id)
       @corp_id = corp_id
-      @isv_mode = false
+      @isv_mode = true
       @corp_secret = ''
+      @permanent_code = ''
     end
   end
 end
